2443ea9a3a9484d4e27e260d0acf9e55e6eaca25
windht <517838178@qq.com>
2016-07-15 18:34:31 +0800

    PAGE EDIT
    TWO TABS AND THREE SAMLL HTML

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,6 +19,9 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+
+
+  
 })
 .controller('ShowCtrl', function($scope) {})
 
